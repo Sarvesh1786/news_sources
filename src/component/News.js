c9bf1281841c0fbdbf59f62af066e2ccf75ef155
Sarvesh1786 <sarveshsharmaiit@gmail.com>
2022-11-28 17:42:13 +0530

    update

---
+++ src/component/News.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function News() {
+  const [state, setState] = useState([]);
+
+  function onClickHandler() {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=5f6540a1b63140d382d8a8f301a898f1"
+      )
+      .then((response) => {
+        // console.log(response);
+        setState(response.data.articles);
+      });
+
+    // console.log("its work your button ");
+  }
+
+  return (
+    <>
+      <div className="container my-3">
+        <div className="row">
+          <div className="col-4">
+            <button className="btn btn-primary" onClick={onClickHandler}>
+              Current News
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container my-3">
+        <div className="row">
+          {state.map((value) => {
+            return (
+              <div className="col-4">
+                <div className="card" style={{ width: "18rem" }}>
+                  <img
+                    src={value.urlToImage}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{value.title}</h5>
+                    <p className="card-text">{value.description}</p>
+                    <a href={value.url} className="btn btn-primary">
+                      Go more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+export default News;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import News from './component/News';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <News />
+
   </React.StrictMode>
 );
 
